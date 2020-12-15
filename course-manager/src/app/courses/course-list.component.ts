a07a4f3bdb4989053d6270781defeb48b53ebf90
jcelsocosta <jccl@cin.ufpe.br>
2020-12-15 11:43:58 -0300

    criacao de uma tabela para exibir dados no HTML

--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {Course} from './course';
 @Component({
     selector: 'app-course-list',
     templateUrl: './course-list.component.html',
 
 })
-export class CourseListComponent{
+export class CourseListComponent implements OnInit{
+
+    courses: Course[];
+
+    constructor(){
+        this.courses = [];
+    }
+    ngOnInit(){
+        this.courses = [
+            {
+                id: 1 ,
+                name: "Angular: Forms",
+                imageUrl: "",
+                preco: 99.99 ,
+                code: 'XPS-8796' ,
+                duration: 120,
+                rating: 4.4,
+                releaseDate: '20/05/2020'
+
+            },
+            {
+                id: 2 ,
+                name: "Angular: HTTP",
+                imageUrl: "",
+                preco: 45.99 ,
+                code: 'LKL-8796' ,
+                duration: 80,
+                rating: 4.6,
+                releaseDate: '02/12/2019'
+            },
+        ]
+    }
 
 }
---
+++ course-manager/src/app/courses/course.ts
@@ -0,0 +1,11 @@
+export class Course{
+    id: number;
+    name: string;
+    imageUrl: string;
+    preco: number;
+    code: string;
+    duration: number;
+    rating: number;
+    releaseDate: string;
+
+}
