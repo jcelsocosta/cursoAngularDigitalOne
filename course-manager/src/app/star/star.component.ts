897dd480d76725c364b64d8cff06b1dc6c5443d3
jcelsocosta <jccl@cin.ufpe.br>
2020-12-15 12:51:11 -0300

    adicionando estrela atraves do calculo de rating

--- course-manager/src/app/star/star.component.ts
+++ course-manager/src/app/star/star.component.ts
@@ -4,6 +4,7 @@ import { Component, OnChanges, Input } from '@angular/core';
 @Component({
     selector: 'app-star',
     templateUrl: './star.component.html',
+    styleUrls:['./star.component.css']
 })
 export class StarComponent implements OnChanges{
 
@@ -13,7 +14,7 @@ export class StarComponent implements OnChanges{
     starWidth: number;
 
     ngOnChanges(): void {
-        this.starWidth = this.rating * 94 / 5;
+        this.starWidth = this.rating * 74 / 5;
     }
 
 }
