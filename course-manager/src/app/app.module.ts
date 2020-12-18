eb3eed911dbff866f6c57e42f8192cce05df9245
jcelsocosta <jccl@cin.ufpe.br>
2020-12-18 13:12:59 -0300

    conhecendo a pasta shared

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -4,9 +4,7 @@ import {FormsModule} from '@angular/forms'
 import {RouterModule} from '@angular/router'
 import { HttpClientModule} from '@angular/common/http';
 
-import { StarComponent } from '../app/star/star.component';
 import { AppComponent } from './app.component';
-import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './404/error404.component';
 import { CourseModule } from './courses/course.module';
@@ -15,8 +13,6 @@ import { CourseModule } from './courses/course.module';
 @NgModule({
   declarations: [
     AppComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
     Error404Component,
   ],
--- course-manager/src/app/courses/course.module.ts
+++ course-manager/src/app/courses/course.module.ts
@@ -4,16 +4,21 @@ import { CourseInfoComponent } from './course-info.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { StarModule } from '../shared/component/star/star.module';
+import { AppPipeModule } from '../shared/pipe/app-pipe.module';
 
 
 @NgModule({
     declarations:[
         CourseListComponent,
-        CourseInfoComponent
+        CourseInfoComponent,
+
     ],
     imports:[
         FormsModule,
         CommonModule,
+        StarModule,
+        AppPipeModule,
         RouterModule.forChild([
             {
                 path: 'courses', component: CourseListComponent
